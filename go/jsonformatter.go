a90be99f82e0e8c48376d5d32fa0be75aa51697e
Pedro Araujo <pedro.araujo@blip.pt>
2015-07-09 16:31:01 +0100

    go: initial implementation

---
+++ go/jsonformatter.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	var ir interface{}
+	var b []byte
+	switch len(os.Args) {
+	case 2:
+		buf, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		} else {
+			b = buf
+		}
+	}
+	err := json.Unmarshal(b, &ir)
+	if err != nil {
+		err := json.Unmarshal(b[strings.Index(string(b), "(")+1:len(b)-3], &ir)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+	}
+	json, _ := json.MarshalIndent(ir, "", "  ")
+	fmt.Print(string(json))
+}
